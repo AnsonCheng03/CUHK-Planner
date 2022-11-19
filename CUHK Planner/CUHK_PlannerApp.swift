27910a7a701f6c8c0fc43b1133487469f33358d7
Anson Cheng <Anson.12666@icloud.com>
2022-11-20 00:12:16 +0800

    Initial Commit

---
+++ CUHK Planner/CUHK_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CUHK_PlannerApp.swift
+//  CUHK Planner
+//
+//  Created by Anson Cheng on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CUHK_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
