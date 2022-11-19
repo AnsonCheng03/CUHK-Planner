5bd6c309d44a3d94088902bb88c43442ed58cc3b
Anson Cheng <Anson.12666@icloud.com>
2022-11-20 02:58:10 +0800

    Create Login View

--- CUHK Planner/ContentView.swift
+++ CUHK Planner/ContentView.swift
@@ -8,12 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("emblem")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width / 3)
+            Spacer()
+                .frame(height: 50)
+            
+            
+            Text("Login")
+                .font(.headline)
+            TextField("SID", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                .padding()
+            TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                .padding()
+            
+            
+            Spacer()
+                .frame(height: 25)
+            Button(action: {
+                    print("sign up bin tapped")
+                }) {
+                    Text("Login")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .font(.system(size: 18))
+                        .padding()
+                        .foregroundColor(.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white, lineWidth: 2)
+                    )
+                }
+                .background(Color(red: 230 / 255, green: 70 / 255, blue: 140 / 255))
+                .cornerRadius(10)
+            Button("Use without login") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
         .padding()
     }
